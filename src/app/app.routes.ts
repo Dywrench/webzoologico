5814a6f3bbe9219a00790e66e000ee52c98c6869
Dywrench <jv88611@gmail.com>
2025-10-29 14:40:49 -0500

    primeras modificaciones

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { Prueba } from './components/prueba/prueba';
+import { AnimalComponent } from './components/animal-component/animal-component';
+
+export const routes: Routes = [
+    { path: 'inicio', component: AnimalComponent }
+
+];
